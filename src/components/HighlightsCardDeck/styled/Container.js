31d0334bd8def764128eabfc4cab4c6ce02675a5
hendra.sadewa <hendra.sadewa@dattabot.io>
2020-04-24 15:58:08 +0700

    handle card deck responsive

--- src/components/HighlightsCardDeck/styled/Container.js
+++ src/components/HighlightsCardDeck/styled/Container.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import breakpoints from '../../../configs/breakpoints';
 
 const Container = styled.div`
   display: grid;
@@ -7,6 +8,18 @@ const Container = styled.div`
   gap: 16px 16px;
 
   padding: 1rem 0;
+
+  /* Mobile */
+  @media screen and (max-width: ${breakpoints.MOBILE}) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+  }
+
+  /* Tablet */
+  @media screen and (min-width: ${breakpoints.MOBILE}) and (max-width: ${breakpoints.TABLET}) {
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+  }
 `;
 
 export default Container;
